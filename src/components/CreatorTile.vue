320eff9dca399db6de36089a58e7b9f1da92470b
maisiemay06 <73712593+maisiemay06@users.noreply.github.com>
2022-06-22 12:29:15 +0100

    creator mobile view

--- src/components/CreatorTile.vue
+++ src/components/CreatorTile.vue
@@ -1,6 +1,8 @@
 <template>
   <div
     class="
+      basis-[47%]
+      sm:basis-[30%]
       bg-white
       rounded-tl-2xl rounded-br-2xl
       w-[15%]
@@ -12,13 +14,27 @@
     <img
       :src="require(`../assets/imgs/${creator.profileImg}`)"
       :alt="creator.name"
-      class="w-full h-auto rounded-tl-2xl rounded-br-2xl z-10"
+      class="
+        w-full
+        h-auto
+        rounded-tl-2xl rounded-br-2xl
+        z-10
+        object-cover object-center
+      "
     />
-    <div class="text-sm p-4 leading-4">
-      <p class="text-[15px] font-semibold">{{ creator.name }}</p>
+
+    <!-- Main Text -->
+    <div class="text-sm sm:p-4 pt-1 leading-4">
+      <p
+        class="text-lg sm:text-[15px] font-semibold mb-2 sm:mb-0 leading-tight"
+      >
+        {{ creator.name }}
+      </p>
       <p class="mb-2">{{ creator.location }}</p>
-      <p class="mb-2">{{ creator.job }}</p>
-      <p class="font-semibold mb-3">{{ creator.company }}</p>
+      <p class="mb-5 sm:mb-2">{{ creator.job }}</p>
+      <p class="font-semibold mb-3 hidden sm:block">
+        {{ creator.company }}
+      </p>
       <div>
         <i class="fa-solid fa-star inline text-meeow-secondary mr-2"></i>
         <p class="font-semibold inline mr-1">{{ creator.rank }}</p>
