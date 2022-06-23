e0a8df836a80bb0d373dff1749ed3bb49f24bd0f
maisiemay06 <73712593+maisiemay06@users.noreply.github.com>
2022-06-23 16:26:55 +0100

    add title section and hide on mobile

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
 <template>
+  <!-- Large Ad -->
   <advert-full></advert-full>
+
+  <!-- Meeting & Creator Tiles -->
   <div class="p-6 sm:p-12">
     <meeting-tiles></meeting-tiles>
     <creator-tiles></creator-tiles>
   </div>
+
+  <!-- Section Title -->
+  <div class="p-6 sm:p-12 hidden sm:block">
+    <h1
+      class="sm:text-6xl text-4xl tracking-tight font-semibold text-left mb-10"
+    >
+      New business, collaboration, support, ideas, opinion, insight, learning,
+      promotion, understanding, opportunies...
+    </h1>
+    <h4 class="sm:text-2xl text-lg text-left mb-10">
+      Find them all, on Meeow.
+    </h4>
+    <button
+      class="bg-meeow-primary rounded-full px-8 py-3 text-sm text-white mr-5"
+    >
+      Join Meeow
+    </button>
+    <button class="border border-black rounded-full px-8 text-sm py-3">
+      Create your own Meeow
+    </button>
+  </div>
+
+  <!-- Small Ads -->
   <div class="flex sm:p-12 gap-8 advert-small">
     <advert-small adImg="ad-img-2.png"></advert-small>
     <advert-small adImg="ad-img-3.png"></advert-small>
@@ -32,12 +58,14 @@ export default {
   font-family: sofia-pro, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   background-color: #fff;
   width: 100%;
   height: 100%;
-
   max-width: 100vw;
+}
+
+.advert-small {
+  flex-direction: column;
 }
 
 @media screen and (min-width: 640px) {
