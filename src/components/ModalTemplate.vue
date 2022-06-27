d4f6f420d29c0aec32296b94a9982e84275d48ad
maisiemay06 <73712593+maisiemay06@users.noreply.github.com>
2022-06-27 15:20:10 +0100

    on demand and point help popups

---
+++ src/components/ModalTemplate.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    class="
+      w-full
+      h-[100vh]
+      bg-black/80
+      absolute
+      top-0
+      flex
+      justify-center
+      items-center
+    "
+    @click="closeModal"
+  >
+    <div
+      class="bg-white rounded-tl-2xl rounded-br-2xl text-left text-sm w-4/5 p-6"
+    >
+      <p>{{ modalContent }}</p>
+      <button
+        class="
+          bg-meeow-primary
+          text-white
+          px-7
+          py-2
+          text-[15px]
+          rounded-full
+          mt-4
+        "
+        @click="closeModal"
+      >
+        {{ modalBtnContent }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModalTemplate",
+  props: ["closeModal", "modalContent", "modalBtnContent"],
+};
+</script>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- Main Nav -->
-  <nav :class="{ hidden: !showNavbar }">
+  <nav :class="{ 'is-hidden': !showNavbar }">
     <div
       class="
         w-full
@@ -26,7 +26,51 @@
       >
         Join Meeow free
       </button>
-      <i class="fa-solid fa-ellipsis-vertical text-3xl ml-4"></i>
+      <i
+        class="fa-solid fa-ellipsis-vertical text-3xl ml-4"
+        @click="toggleNavMenu"
+      ></i>
+    </div>
+
+    <!-- Nav Menu -->
+    <div
+      v-if="showNavMenu"
+      class="
+        z-20
+        absolute
+        bg-white
+        right-0
+        h-[100vh]
+        w-2/3
+        drop-shadow-xl
+        pt-28
+        pl-10
+        text-left
+        flex flex-col
+        items-start
+        text-lg
+      "
+    >
+      <ul>
+        <li class="py-3">Favourites</li>
+        <li class="py-3">Calendar</li>
+        <li class="py-3">Messaging</li>
+        <li class="py-3">Tell a Friend</li>
+        <li
+          class="py-3"
+          @click="
+            () => {
+              openModal('points');
+              toggleNavMenu();
+            }
+          "
+        >
+          Points
+        </li>
+      </ul>
+      <button class="mt-auto pb-10 flex items-center" @click="toggleNavMenu">
+        Close <i class="fa-solid fa-xmark text-3xl ml-2"></i>
+      </button>
     </div>
 
     <!-- Search Bar -->
@@ -71,16 +115,37 @@
     >
       <p class="text-3xl font-semibold">13:52</p>
       <button class="text-sm">Open on-demand networking</button>
-      <img src="../assets/imgs/info-bubble.png" alt="" />
+      <img
+        src="../assets/imgs/info-bubble.png"
+        alt=""
+        @click="() => openModal('ondemand')"
+      />
     </div>
+
+    <modal-template
+      v-if="showModal"
+      class="z-30"
+      :closeModal="closeModal"
+      :modalContent="modalContent"
+      :modalBtnContent="modalBtnContent"
+    ></modal-template>
   </nav>
 </template>
 
 <script>
+import ModalTemplate from "./ModalTemplate.vue";
+
 export default {
   name: "NavBar",
+  components: {
+    ModalTemplate,
+  },
   data() {
     return {
+      showNavMenu: false,
+      showModal: false,
+      modalContent: "",
+      modalBtnContent: "",
       showNavbar: true,
       lastScrollPos: 0,
     };
@@ -92,6 +157,34 @@ export default {
     window.removeEventListener("scroll", this.onScroll);
   },
   methods: {
+    toggleNavMenu() {
+      console.log("show");
+      this.showNavMenu = !this.showNavMenu;
+    },
+    openModal(props) {
+      if (props === "points") {
+        this.modalContent = `Meeow awards every member points for every engagement with the community, helping you to build your reputation as a brilliant networker.
+        <br />
+        <br />
+        As you build your points total and rise through the ranks tobe an
+        Ambassador`;
+        this.modalBtnContent = "Join now";
+      } else if (props === "ondemand") {
+        this.modalContent = `AI matched, On-demand networking on Meeow allows you to network with up to 3 new people on the hour, every hour for 45 mins.
+        <br />
+        <br />
+        Only 4 people in each meeting, to ensure good quality conversation.
+        <br />
+        <br />
+        No limit to the number of 4 person meetings at one time, so everyone gets to network when they want to.
+        `;
+        this.modalBtnContent = "Open on-demand networking";
+      }
+      this.showModal = true;
+    },
+    closeModal() {
+      this.showModal = false;
+    },
     onScroll() {
       const currentScrollPos =
         window.pageYOffset || document.documentElement.scrollTop;
@@ -100,7 +193,7 @@ export default {
         return;
       }
 
-      if (Math.abs(currentScrollPos - this.lastScrollPos) < 100) {
+      if (Math.abs(currentScrollPos - this.lastScrollPos) < 80) {
         return;
       }
 
@@ -113,4 +206,11 @@ export default {
 </script>
 
 <style scoped>
+nav {
+  transform: translateY(0);
+  transition: transform 300ms ease-out;
+}
+nav.is-hidden {
+  transform: translateY(-100%);
+}
 </style>
